04562b46766fa31403d4e2d7ddf89c004a4c95e0
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-09-12 21:38:54 +0300

    add ci and settings add local pom dependency

--- src/main/java/ru/xodavit/simple/consumer/Main.java
+++ src/main/java/ru/xodavit/simple/consumer/Main.java
@@ -1,7 +1,9 @@
 package ru.xodavit.simple.consumer;
 
+import ru.xodavit.http.server.Helper;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello from app");
+        System.out.println(Helper.help());
     }
 }
