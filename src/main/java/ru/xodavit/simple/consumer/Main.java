952dfed8604df4e7ec57112101682c1b4c98a83c
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-09-13 18:45:35 +0300

    changed main

--- src/main/java/ru/xodavit/simple/consumer/Main.java
+++ src/main/java/ru/xodavit/simple/consumer/Main.java
@@ -1,10 +1,33 @@
 package ru.xodavit.simple.consumer;
 
-import ru.xodavit.http.server.Helper;
+
+import ru.xodavit.http.server.framework.HttpServer;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Helper.help());
-        System.out.println(Helper.help2());
+        new Thread(() -> {
+            try {
+                ru.xodavit.http.server.app.Main.main(args);
+            } catch ( IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        HttpServer server = null;
+        try {
+           server = new HttpServer(9998);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        server.listen();
+        System.out.println(server.isStopped());
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(server.isStopped());
     }
 }
